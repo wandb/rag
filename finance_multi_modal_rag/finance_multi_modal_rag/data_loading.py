f3896f561883afacc7046d0545c97b6ad040312d
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-09-26 19:16:43 +0530

    add: MultiModalPredictor

--- finance_multi_modal_rag/finance_multi_modal_rag/data_loading.py
+++ finance_multi_modal_rag/finance_multi_modal_rag/data_loading.py
@@ -1,25 +1,60 @@
+import base64
+import io
 import os
 
 import filetype
 from edgar import Company
 from PIL import Image
 from pydantic import BaseModel
-from rich.progress import track
+from tqdm.auto import tqdm
+
+from .llm_wrapper import MultiModalPredictor
+
+
+def encode_image(image: Image.Image) -> str:
+    byte_arr = io.BytesIO()
+    image.save(byte_arr, format="PNG")
+    encoded_string = base64.b64encode(byte_arr.getvalue()).decode("utf-8")
+    encoded_string = f"data:image/png;base64,{encoded_string}"
+    return str(encoded_string)
 
 
 class EdgarDataLoader(BaseModel):
     company_name: str
+    image_description_generator: MultiModalPredictor
+
+    def generate_image_description(
+        self, images: list[Image.Image], filing_data: str
+    ) -> list[str]:
+        return [
+            self.image_description_generator.predict(
+                user_prompts=[
+                    f"""You are an expert finacial analyst tasked with genrating
+desciptions for images in financial filings. If the image has text in it, you should first
+generate a description of the image and then extract the text in markdown format.
+
+Here is the financial filing:
+
+---
+{filing_data}
+---""",
+                    encode_image(image),
+                ],
+            )
+            for image in tqdm(images, desc="Generating image descriptions:")
+        ]
 
     def load_data(
         self,
         form_type: str,
         upload_images: bool = True,
+        upload_image_descriptions: bool = True,
     ) -> list[dict[str, str]]:
         filings_10q = Company(self.company_name).get_filings(form=[form_type])
         filings_data = []
-        for filing in track(
+        for filing in tqdm(
             filings_10q,
-            description=f"Fetching {form_type} filings for {self.company_name}",
+            desc=f"Fetching {form_type} filings for {self.company_name}",
         ):
 
             current_filing_data = {
@@ -30,7 +65,6 @@ class EdgarDataLoader(BaseModel):
                 "content": filing.markdown(),
             }
 
-            attachment_texts = []
             current_filing_data["images"] = []
             for idx, attachment in enumerate(filing.attachments):
                 os.makedirs(
@@ -49,7 +83,13 @@ class EdgarDataLoader(BaseModel):
                         current_filing_data["images"].append(image)
                     except Exception:
                         pass
-            current_filing_data["attachment_texts"] = attachment_texts
+
+            if upload_image_descriptions:
+                current_filing_data["image_descriptions"] = (
+                    self.generate_image_description(
+                        current_filing_data["images"], current_filing_data["content"]
+                    )
+                )
 
             filings_data.append(current_filing_data)
 
