86a79fa1d35e99ef2a1980231a82834688562cb8
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-09-30 18:09:56 +0530

    update: chunking + retrieval

--- finance_multi_modal_rag/test.py
+++ finance_multi_modal_rag/test.py
@@ -1,24 +1,12 @@
 import weave
 from dotenv import load_dotenv
 
-from finance_multi_modal_rag.llm_wrapper import MultiModalPredictor
-from finance_multi_modal_rag.response_generation import FinanceQABot
-from finance_multi_modal_rag.retrieval import BGERetriever
+from finance_multi_modal_rag.chunking import chunk_documents
 
 load_dotenv()
 
 weave.init(project_name="finance_multi_modal_rag")
-retriever = BGERetriever.from_wandb_artifact(
-    artifact_address="geekyrakshit/finance_multi_modal_rag/tsla-index:latest",
-    weave_chunked_dataset_address="TSLA_sec_filings_chunks:v1",
-    model_name="BAAI/bge-small-en-v1.5",
+chunk_documents(
+    source_dataset_address="TSLA_sec_filings:v8",
+    target_dataset_name="TSLA_sec_filings_chunks",
 )
-finace_qa_bot = FinanceQABot(
-    predictor=MultiModalPredictor(
-        model_name="meta-llama/Llama-3.2-90B-Vision-Instruct-Turbo",
-        base_url="http://195.242.25.198:8032/v1",
-    ),
-    retriever=retriever,
-    weave_corpus_dataset_address="TSLA_sec_filings:v8",
-)
-finace_qa_bot.predict(query="what did elon say in the tweets that tesla reported?")
